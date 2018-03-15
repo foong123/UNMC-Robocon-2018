4d219795bda2dcf1d83459acc141991662b5daad
AlexTayUNMC <kecy6tgy@nottingham.edu.my>
2018-03-15 10:26:43 +0800

    Added frequency control and change duty cycle control (0 to 100)

--- Launching/raspi_servo_test.py
+++ Launching/raspi_servo_test.py
@@ -3,22 +3,23 @@ import time
 GPIO.setmode(GPIO.BCM)
 pwm_pin = 18
 GPIO.setup(pwm_pin, GPIO.OUT)
-pwm=GPIO.PWM(pwm_pin, 50)
-pwm.start(0)
 
-for i in range(360)
-	SetAngle(45,i) 
-	print(i)
+for f in range(50,330,25)
+	pwm=GPIO.PWM(pwm_pin, f)
+	pwm.start(0)
+	for i in range(100)
+		SetAngle(i) 
+		print(i)
 	
 	
 pwm.stop()
 
 
 
-def SetAngle(angle,i):
-	duty = angle / (i)
+def SetAngle(duty):
+	#duty = angle / (i)
 	GPIO.output(pwm_pin, True)
-	pwm.ChangeDutyCycle(duty)
-	time.sleep(1)
+	pwm.ChangeDutyCycle(duty)# where 0.0 <= dc <= 100.0
+	time.sleep(0.5)
 	GPIO.output(pwm_pin, False)
 	pwm.ChangeDutyCycle(0)
