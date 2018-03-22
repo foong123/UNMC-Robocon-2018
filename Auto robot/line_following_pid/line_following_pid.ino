f5eee024db9744a4c6ef36f3aa08d8db5fbb873b
Low-ZheXian <zx_low@hotmail.com>
2018-03-23 00:44:24 +0800

    added line following code with PID

---
+++ Auto robot/line_following_pid/line_following_pid.ino
@@ -0,0 +1,208 @@
+#include <Wire.h>
+#define uchar unsigned char
+
+//for sunfounder
+uchar t;
+uchar data[16];
+float sumvalueweight;
+float sumvalue;
+long WA;
+float d;
+
+float offset[8] = {1, 1.02, 1.11, 0.99, 0.81, 0.88, 0.82, 0.89};
+
+//for pid
+float Kp = 10.0;
+float Ki = 0.025;
+float Kd = 25;
+float output;
+float error, errorSum, errorOld;
+
+//for motor movement
+int leftMotorBaseSpeed = 25;
+int rightMotorBaseSpeed = 25;
+int min_speed = -35;
+int max_speed = 45;
+float leftMotorSpeed = 0;  // Initialise Speed variables
+float rightMotorSpeed = 0;
+
+//pin for motor
+int enL = 5;
+int dirL = 6;
+int enR = 7;
+int dirR = 8;
+
+void setup() {
+  Wire.begin();
+  Serial.begin(9600);
+
+  error = 0;    // Initialise error variables
+  errorSum = 0;
+  errorOld = 0;
+
+  pinMode(enL, OUTPUT);
+  pinMode(enR, OUTPUT);
+  pinMode(dirL, OUTPUT);
+  pinMode(dirR, OUTPUT);
+}
+
+void loop() {
+  WA = weightedAverage();
+  output = pid(WA);
+  motor_speed(output);
+}
+
+float weightedAverage() {
+  Serial.println("bbbbb");
+  Wire.requestFrom(9, 16); //request 16 bytes from slave device #9
+  while (Wire.available())
+  {
+    data[t] = Wire.read();
+    if (t < 15) {
+      t++;
+    }
+    else {
+      t = 0;
+    }
+  }
+  Serial.print("data[1]:");
+  Serial.println(data[0] * offset[0]);
+  Serial.print("data[2]:");
+  Serial.println(data[2] * offset[1]);
+  Serial.print("data[3]:");
+  Serial.println(data[4] * offset[2]);
+  Serial.print("data[4]:");
+  Serial.println(data[6] * offset[3]);
+  Serial.print("data[5]:");
+  Serial.println(data[8] * offset[4]);
+  Serial.print("data[6]:");
+  Serial.println(data[10] * offset[5]);
+  Serial.print("data[7]:");
+  Serial.println(data[12] * offset[6]);
+  Serial.print("data[8]:");
+  Serial.println(data[14] * offset[7]);
+
+  sumvalueweight = ((data[0] * (-42) * offset[0]) + (data[2] * (-30) * offset[1]) + (data[4] * (-18) * offset[2]) + (data[6] * (-6) * offset[3]) + (data[8] * 6 * offset[4]) + (data[10] * 18 * offset[5]) + (data[12] * 30 * offset[6]) + (data[14] * 42 * offset[7]));
+  sumvalue = ((data[0] * offset[0]) + (data[2] * offset[1]) + (data[4] * offset[2]) + (data[6] * offset[3]) + (data[8] * offset[4]) + (data[10] * offset[5]) + (data[12] * offset[6]) + (data[14] * offset[7]));
+  d = (sumvalueweight) / (sumvalue);
+  Serial.println(d);
+  return d;
+
+  if (sumvalue >= 560.0) {
+    turn90();
+  }
+}
+
+float pid(long lineDist)
+{
+  errorOld = error;        // Save the old error for differential component
+  error = lineDist;  // Calculate the error in position
+  errorSum += error;
+  //Serial.println(error);
+
+  float proportional = error * Kp;  // Calculate the components of the PID
+
+  float integral = errorSum * Ki;
+
+  float differential = (error - errorOld) * Kd;
+
+  long output = proportional + integral + differential;  // Calculate the result
+
+  return output;
+}
+
+void motor_speed(float output)
+{
+  leftMotorSpeed = leftMotorBaseSpeed + output;
+  rightMotorSpeed = rightMotorBaseSpeed - output;
+  Serial.println("aaaaa");
+
+  if (leftMotorSpeed > 23)
+  {
+    leftMotorSpeed = constrain(leftMotorSpeed, min_speed, max_speed);
+    //MOTOR.setSpeedDir1(abs(leftMotorSpeed), DIRR); Forward
+    motorLeft(leftMotorSpeed, 1);
+    Serial.print(leftMotorSpeed);
+    Serial.print("   ");
+  }
+  else
+  {
+    leftMotorSpeed = constrain(leftMotorSpeed, min_speed, max_speed );
+    //MOTOR.setSpeedDir1(abs(leftMotorSpeed), DIRF); reverse
+    motorLeft(leftMotorSpeed, 0);
+    Serial.print(leftMotorSpeed);
+    Serial.print("   ");
+  }
+
+  if (rightMotorSpeed > 23)
+  {
+    rightMotorSpeed = constrain(rightMotorSpeed, min_speed, max_speed);
+    //MOTOR.setSpeedDir2(abs(rightMotorSpeed), DIRF);
+    motorRight(rightMotorSpeed, 1);
+    Serial.println(rightMotorSpeed);
+  }
+  else
+  {
+    rightMotorSpeed = constrain(rightMotorSpeed, min_speed, max_speed);
+    //MOTOR.setSpeedDir2(abs(rightMotorSpeed), DIRR);
+    motorRight(rightMotorSpeed, 0);
+    Serial.println(rightMotorSpeed);
+  }
+
+}
+
+void motorLeft(float speed_pwm, int dir) {
+  digitalWrite(dirL, dir);
+  analogWrite(enL, speed_pwm);
+}
+
+void motorRight(float speed_pwm, int dir) {
+  digitalWrite(dirR, dir);
+  analogWrite(enR, speed_pwm);
+}
+
+void turn90() {
+  digitalWrite(dirL, LOW);
+  analogWrite(enL, 100);
+  digitalWrite(dirR, HIGH);
+  analogWrite(enR, 100);
+  check90();
+}
+
+void motorStop() {
+  digitalWrite(dirL, HIGH);
+  analogWrite(enL, 0);
+  digitalWrite(dirR, HIGH);
+  analogWrite(enR, 0);
+}
+
+void check90() {
+  Wire.requestFrom(9, 16); //request 16 bytes from slave device #9
+  while (Wire.available())
+  {
+    data[t] = Wire.read();
+    if (t < 15) {
+      t++;
+    }
+    else {
+      t = 0;
+    }
+  }
+  float rightMost = (data[14] * offset[7]);
+  float leftMost = (data[0] * offset[1]);
+  
+  while (1) {
+    if (rightMost < 70 && leftMost < 70) {
+      break;
+    }
+  }
+  while (1) {
+    if (rightMost >= 70 && leftMost >= 70) {
+      break;
+    }
+  }
+  motorStop();
+}
+
+
+
